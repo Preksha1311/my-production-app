5dd18742d3c05f755f8791fa76899d5772e7d156
Preksha <prekshaparas1311@gmail.com>
2024-06-02 18:59:15 +0530

    update

--- src/app/categories/[CategoryDetails]/page.tsx
+++ src/app/categories/[CategoryDetails]/page.tsx
@@ -1,13 +1,15 @@
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-// export default function CategoryDetails({ params }: { params: { categoryId: string } }) {
-//   const router = useRouter()
-//   // let id= params.categoryId;
+export default function CategoryDetails({ params }: { params: { categoryId: string } }) {
+  const router = useRouter()
+  // let id= params.categoryId;
 //   console.log(router.)
-//   return (
-//     <>
-//       <h1>Id is{router.query.slug}</h1>
-//       <p>Details of category {router.query.slug}</p>
-//     </>
-//   );
-// }
+  return (
+    <>
+      {/* <h1>Id is{router.query.slug}</h1> */}
+      {/* <p>Details of category {router.query.slug}</p>
+       */}
+       <h1>hii</h1>
+    </>
+  );
+}
